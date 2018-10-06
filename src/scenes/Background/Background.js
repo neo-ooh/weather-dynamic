15c21df1d3a8b3d6135e8862d1f8138e1be4c12e
Val <valentin@dufois.fr>
2018-10-05 22:28:40 -0400

    So, yeah, *drags on his cigarettes*, I made it work on the first time*

--- src/scenes/Background/Background.js
+++ src/scenes/Background/Background.js
@@ -49,7 +49,6 @@ export default class background extends Component {
     let location
 
     if (this.props.player.isBroadSign) {
-      this.props.log(window.BroadSignObject.display_unit_lat_long)
       location = {
         lat: window.BroadSignObject.display_unit_lat_long.split(',')[0],
         lng: window.BroadSignObject.display_unit_lat_long.split(',')[1]
