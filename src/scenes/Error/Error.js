8dc15dd7dfd95398b661d8e93280817cd062ad32
Val <valentin@dufois.fr>
2018-11-05 13:43:09 -0500

    Adding black fallbackscreen in case of error on production end

--- src/scenes/Error/Error.js
+++ src/scenes/Error/Error.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 
 export default class Error extends Component {
   render () {
+    if(process.env.REACT_APP_ENV !== 'production') {
+      return (
+        <section id="error-screen">
+          <h1>Weather Dynamic</h1>
+          <h3>An error as occurred</h3>
+          <h6>v0.403 - { window.location.href }</h6>
+          <h5>{ this.props.message }</h5>
+          <h5 className="stack">{ this.props.stack || '' }</h5>
+        </section>
+      )
+    }
+
     return (
-      <section id="error-screen">
-        <h1>Weather Dynamic</h1>
-        <h3>An error as occurred</h3>
-        <h6>v0.403 - { window.location.href }</h6>
-        <h5>{ this.props.message }</h5>
-        <h5 className="stack">{ this.props.stack || '' }</h5>
+      <section id="neo-logo-screen">
       </section>
     )
   }
